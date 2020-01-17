5a07444ff9320d2867aa892615368ef665b5c1fa
idawud <ismaildawud96@gmail.com>
2020-01-17 11:07:38 +0000

    removed unused imports

--- src/main/java/io/turntabl/AwsAccountManagementApplication.java
+++ src/main/java/io/turntabl/AwsAccountManagementApplication.java
@@ -1,11 +1,7 @@
 package io.turntabl;
 
-import io.turntabl.services.Roles;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class AwsAccountManagementApplication {
