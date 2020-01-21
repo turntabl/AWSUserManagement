727852756bd1117d6aa2effb88d69caa1e3d374c
idawud <ismaildawud96@gmail.com>
2020-01-21 17:37:28 +0000

    feat
    removing aws-arn complete

--- src/main/java/io/turntabl/services/GSuite.java
+++ src/main/java/io/turntabl/services/GSuite.java
@@ -1,21 +1,20 @@
-package io.turntabl;
+package io.turntabl.services;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.ArrayMap;
 import com.google.api.services.admin.directory.Directory;
 import com.google.api.services.admin.directory.DirectoryScopes;
 import com.google.api.services.admin.directory.model.User;
-import com.google.api.services.admin.directory.model.UserPhone;
 import com.google.common.collect.ImmutableList;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GSuite {
     private static final String APPLICATION_NAME = "Turntabl G suite - AWS Role Management";
@@ -28,8 +27,120 @@ public class GSuite {
     private static final List<String> SCOPES = ImmutableList.of( DirectoryScopes.ADMIN_DIRECTORY_USER_READONLY, DirectoryScopes.ADMIN_DIRECTORY_USER);
     private static final String CREDENTIALS_FILE_PATH = "aws-account-management-265416-84d47f654b81.json";
 
+    public static Map<String, String> fetchAllUsers(){
+        Map<String, String> users = new HashMap<>();
+        try {
+            fetchAllUserNameAndIds(users);
+            return users;
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+            return users;
+        }
+    }
 
-    public static void main(String... args) throws IOException, GeneralSecurityException {
+    private static void fetchAllUserNameAndIds(Map<String, String> users) throws IOException, GeneralSecurityException {
+        Directory service = getDirectory();
+        Directory.Users.List usersInDomain = service.users().list().setDomain("turntabl.io").setProjection("full");
+        List<User> userList = usersInDomain.execute().getUsers();
+
+        userList.forEach(user -> {
+            if ( user.getOrgUnitPath().equals("/GH Tech")) {
+                users.put(user.getName().getFullName(), user.getId());
+            }
+        });
+    }
+
+
+    public static Map<String, User> fetchAllUserInfo() throws IOException, GeneralSecurityException {
+        Map<String, User> users = new HashMap<>();
+        Directory service = getDirectory();
+        Directory.Users.List usersInDomain = service.users().list().setDomain("turntabl.io").setProjection("full");
+        List<User> userList = usersInDomain.execute().getUsers();
+
+        userList.forEach(user -> {
+            if ( user.getOrgUnitPath().equals("/GH Tech")) {
+                users.put(user.getId(), user);
+            }
+        });
+        return users;
+    }
+
+    public static boolean addAWSARN(String userId, String awsArn){
+        try {
+            Map<String, User> allUserInfo = fetchAllUserInfo();
+            User user = allUserInfo.get(userId);
+
+            Map<String, Map<String, Object>> customSchemas = user.getCustomSchemas();
+            if ( customSchemas != null){
+                Map<String, Object> o = customSchemas.getOrDefault("AWS_SAML", null);
+                if ( o != null) {
+                    List<Map<String, Object>> iam_role = (List<Map<String, Object>>) o.get("IAM_Role");
+
+                    if (iam_role != null) {
+                        iam_role.forEach( role -> {
+                            String value = (String) role.get("value");
+                            List<String> strings = Arrays.asList(value.split(","));
+                            long count = strings.stream().filter(s -> Objects.equals(s.trim(), awsArn)).count();
+                            if ( count == 0){
+                                System.out.println(strings);
+                                System.out.println(awsArn);
+                                String collect = strings.stream().filter(s -> !Objects.equals(s.trim(), awsArn)).collect(Collectors.joining(","));
+                                role.put("value", collect + "," + awsArn);
+                            }
+                        });
+                        Directory service = getDirectory();
+                        service.users().update(userId, user).execute();
+                        return true;
+                        // System.out.println(user);
+                    }
+                    return false;
+                }
+                return false;
+            }
+
+            return false;
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean removeAWSARN(String userId, String awsArn){
+        try {
+            Map<String, User> allUserInfo = fetchAllUserInfo();
+            User user = allUserInfo.get(userId);
+
+            Map<String, Map<String, Object>> customSchemas = user.getCustomSchemas();
+            if ( customSchemas != null){
+                Map<String, Object> o = customSchemas.getOrDefault("AWS_SAML", null);
+                if ( o != null) {
+                    List<Map<String, Object>> iam_role = (List<Map<String, Object>>) o.get("IAM_Role");
+
+                    if (iam_role != null) {
+                        iam_role.forEach( role -> {
+                            String value = (String) role.get("value");
+                            List<String> strings = Arrays.asList(value.split(","));
+                            String collect = strings.stream().filter(s -> !Objects.equals(s.trim(), awsArn)).collect(Collectors.joining(","));
+                            role.put("value", collect);
+                        });
+                        Directory service = getDirectory();
+                        service.users().update(userId, user).execute();
+                        return true;
+                        // System.out.println(user);
+                    }
+                    return false;
+                }
+                return false;
+            }
+
+            return false;
+        } catch (IOException | GeneralSecurityException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static Directory getDirectory() throws IOException, GeneralSecurityException {
         GoogleCredential gcFromJson = GoogleCredential
                                             .fromStream(new FileInputStream(CREDENTIALS_FILE_PATH))
                                             .createScoped(SCOPES);
@@ -45,26 +156,9 @@ public class GSuite {
                 .build();
 
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Directory service = new Directory.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+        return new Directory.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                                         .setApplicationName(APPLICATION_NAME)
                                         .build();
-
-        Directory.Users  usr = service.users();
-        User dawud = usr.get("100262028487240433703").execute();
-
-        /*Object phones = dawud.getPhones();
-        if( phones != null){
-                List<ArrayMap<String,Object>> objects = (List<ArrayMap<String,Object>>)phones;
-                for (ArrayMap<String,Object> object: objects){
-                    for (int i = 0; i < object.size(); i++){
-                        System.out.println(object.getKey(i) + " -> " + object.getValue(i));
-                    }
-                       // id.put(object.getKey(i), object.getValue(i));
-                }
-        }*/
- 
-        // dawud.getIms()
-        /*execute.getUsers().forEach(usr -> System.out.println(usr.getName().getFullName()));*/
     }
 
 }
