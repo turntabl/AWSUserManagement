36794145b623d9c55c41965047eeffbc6ce972ce
idawud <ismaildawud96@gmail.com>
2020-01-22 08:54:46 +0000

    refactor
    removed unnessary whitespaces

--- src/main/java/io/turntabl/AwsAccountManagementApplication.java
+++ src/main/java/io/turntabl/AwsAccountManagementApplication.java
@@ -1,33 +1,13 @@
 package io.turntabl;
 
-import io.turntabl.models.Permission;
-import io.turntabl.services.GSuite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Map;
 
 @SpringBootApplication
 public class AwsAccountManagementApplication {
 
-	public static void main(String[] args) throws IOException, GeneralSecurityException {
-		boolean b = GSuite.removeAWSARN("102924368074115126817", "arn:aws:iam::926377470665:role/gsuite-elasticbeanstalk-fullaccess-role");
-		System.out.println(b);
-
-		/*GSuite.fetchAllUserInfo().forEach( (s, user) -> {
-			if (user.getName().getFullName().contains("Dawud") || user.getName().getFullName().contains("Osei") || user.getName().getFullName().contains("John")){
-				System.out.println(user);
-			}
-		});*/
-
-		// allUsers.forEach((x,y)-> System.out.println(x + " -> " + y));
-        // SpringApplication.run(AwsAccountManagementApplication.class, args);
-
-		/*Permission permission = new Permission("some-role");
-		System.out.println(permission.toMap());
-*/
+	public static void main(String[] args) {
+        SpringApplication.run(AwsAccountManagementApplication.class, args);
 	}
 }
 
