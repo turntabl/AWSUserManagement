0715bc82437eae8f0d430e09353d0dd2aaf40557
idawud <ismaildawud96@gmail.com>
2020-01-22 09:57:03 +0000

    refactor
    removed all unsed deps

--- src/main/java/io/turntabl/AwsAccountManagementApplication.java
+++ src/main/java/io/turntabl/AwsAccountManagementApplication.java
@@ -2,8 +2,10 @@ package io.turntabl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AwsAccountManagementApplication {
 
 	public static void main(String[] args) {
